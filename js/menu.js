f75eeb4137305e6e39f0b3d503be67057102f16e
Fenickss <denis.suxov.1991@mail.ru>
2022-05-29 23:09:04 +0300

    menu

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,21 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
-  
-    menuBtnRef.addEventListener("click", () => {
-      const expanded =
-        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-  
-      menuBtnRef.classList.toggle("is-open");
-      menuBtnRef.setAttribute("aria-expanded", !expanded);
-  
-      mobileMenuRef.classList.toggle("is-open");
-    });
-  })();
+  const menuBtnOpen = document.querySelector("[data-menu-open]");
+  const menuBtnClose = document.querySelector("[data-menu-close]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+
+  menuBtnOpen.addEventListener("click", () => {
+    const expanded = menuBtnOpen.getAttribute("aria-expanded") === "true" || false;
+
+    menuBtnOpen.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  })
+
+  menuBtnClose.addEventListener("click", () => {
+    const expanded = menuBtnOpen.getAttribute("aria-expanded") === "true" || false;
+
+    menuBtnOpen.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  })
+})();
